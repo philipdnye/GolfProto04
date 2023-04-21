348568db64c320e4156a4c1244961097a9e2db2c
Philip Nye <philip@nye.me.uk>
2023-04-21 17:52:52 +0100

    Initial Commit

---
+++ GolfProto04/GolfProto04App.swift
@@ -0,0 +1,17 @@
+//
+//  GolfProto04App.swift
+//  GolfProto04
+//
+//  Created by Philip Nye on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GolfProto04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
